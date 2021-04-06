b9693b4d953c2f1ae5c65bdee45c2a5f03a770e8
SakuraLXY <79798356+SakuraLXY@users.noreply.github.com>
2021-04-06 22:09:56 +0800

    Update testmodel.py

--- testmodel.py
+++ testmodel.py
@@ -30,16 +30,16 @@ def get_labeled_data(picklename):
         data = pickle.load(open('{}.pickle'.format(picklename),'rb'))
     else:
         #un_gz
-        file_name = 't10k-images-idx3-ubyte'
-        f_name = file_name.replace(".gz", "")
-        g_file = gzip.GzipFile(file_name)
-        open(f_name, "wb+").write(g_file.read())
-        g_file.close()
-        file_name = 't10k-labels-idx1-ubyte'
-        f_name = file_name.replace(".gz", "")
-        g_file = gzip.GzipFile(file_name)
-        open(f_name, "wb+").write(g_file.read())
-        g_file.close()
+#         file_name = 't10k-images-idx3-ubyte'
+#         f_name = file_name.replace(".gz", "")
+#         g_file = gzip.GzipFile(file_name)
+#         open(f_name, "wb+").write(g_file.read())
+#         g_file.close()
+#         file_name = 't10k-labels-idx1-ubyte'
+#         f_name = file_name.replace(".gz", "")
+#         g_file = gzip.GzipFile(file_name)
+#         open(f_name, "wb+").write(g_file.read())
+#         g_file.close()
         # Open the images with gzip in read binary mode
         images = open(MNIST_data_path + 't10k-images.idx3-ubyte','rb')
         labels = open(MNIST_data_path + 't10k-labels.idx1-ubyte','rb')
