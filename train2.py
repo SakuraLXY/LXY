eb7de992e1a39e259f4efb17882a0b2a89dbb2d4
SakuraLXY <79798356+SakuraLXY@users.noreply.github.com>
2021-05-04 23:04:54 +0800

    Update train2.py

--- train2.py
+++ train2.py
@@ -26,8 +26,36 @@ def get_labeled_data(picklename):
     """Read input-vector (image) and target class (label, 0-9) and return
        it as list of tuples.
     """
-    os.path.isfile('%s.pickle' % picklename):
-    data = pickle.load(open('{}.pickle'.format(picklename),'rb'))
+    if os.path.isfile('%s.pickle' % picklename):
+        data = pickle.load(open('{}.pickle'.format(picklename),'rb'))
+    else:
+        # Open the images with gzip in read binary mode
+        images = open(MNIST_data_path + 'train-images.idx3-ubyte','rb')
+        labels = open(MNIST_data_path + 'train-labels.idx1-ubyte','rb')
+        # Get metadata for images
+        images.read(4)  # skip the magic_number #跳过了MNIST数据集的魔法数字的四个字节
+        number_of_images = unpack('>I', images.read(4))[0] #num = 60000
+        rows = unpack('>I', images.read(4))[0] #rows = 28
+        cols = unpack('>I', images.read(4))[0] #cols = 28
+        # Get metadata for labels
+        labels.read(4)  # skip the magic_number
+        N = unpack('>I', labels.read(4))[0] #N = 60000
+
+        if number_of_images != N:
+            raise Exception('number of labels did not match the number of images')
+        # Get the data
+        x = np.zeros((N, rows, cols), dtype=np.uint8)  # Initialize numpy array
+        y = np.zeros((N, 1), dtype=np.uint8)  # Initialize numpy array
+
+        for i in range(N):
+            if i % 1000 == 0:
+                print("i: %i" % i) #提示数据读了多少
+
+            x[i] = [[unpack('>B', images.read(1))[0] for unused_col in range(cols)]  for unused_row in range(rows) ]
+            y[i] = unpack('>B', labels.read(1))[0]
+
+        data = {'x': x, 'y': y, 'rows': rows, 'cols': cols}
+        pickle.dump(data, open("%s.pickle" % picklename, "wb"))
 
     return data
 
