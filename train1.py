a936e8535e34d947c85033ea03e7df450e395e9e
SakuraLXY <79798356+SakuraLXY@users.noreply.github.com>
2021-08-07 10:35:16 +0800

    Update train1.py

--- train1.py
+++ train1.py
@@ -344,7 +344,7 @@ print('create connections between X and A ')
 timing_rule = sim.SpikePairRule(tau_plus=8.0, tau_minus=2.0,  # 8,1
                                 A_plus=0.0005, A_minus=0.0005)  # 80,20
 weight_rule = sim.AdditiveWeightDependence(w_max=1, w_min=0)
-last_weight=np.load('nweight.npy').reshape(-1)
+last_weight=np.load('nweight2.npy').reshape(-1)
 # stdp = sim.STDPMechanism(timing_dependence=timing_rule,
 #                          weight_dependence=weight_rule,
 #                          weight=RandomDistribution(distribution='normal_clipped', low=0, high=1, mu=0.5, sigma=0.3),
