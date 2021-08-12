0b7e6886cf46a24b82fc6cbf7b76a5a90a32049e
SakuraLXY <79798356+SakuraLXY@users.noreply.github.com>
2021-08-12 13:13:21 +0800

    Update train1.py

--- train1.py
+++ train1.py
@@ -180,7 +180,7 @@ np.random.seed(0)  # 使得后续生产的随机数可预测
 data_path = './'
 
 weight_path = data_path + 'random/'
-num_examples = 400  #  一次使用训练例子的数量。再多就不行了
+num_examples = 4000  #  一次使用训练例子的数量。再多就不行了
 turns=0 # 这是第几次训练
 
 ending = ''
@@ -370,7 +370,7 @@ print('create connections between X and A ')
 # print("Testing stdp initial weight random generator, rand value = ",str(stdp_initial_weights.next()))
 timing_rule = sim.SpikePairRule(tau_plus=18.0, tau_minus=18.0,  # 8,1
                                 A_plus=0.05, A_minus=0.05)  # 80,20
-weight_rule = sim.AdditiveWeightDependence(w_max=0.2, w_min=0)
+weight_rule = sim.AdditiveWeightDependence(w_max=0.3, w_min=0)
 # last_weight=np.load('normalize_weight.npy').reshape(-1)
 # stdp = sim.STDPMechanism(timing_dependence=timing_rule,
 #                          weight_dependence=weight_rule,
@@ -379,7 +379,7 @@ weight_rule = sim.AdditiveWeightDependence(w_max=0.2, w_min=0)
 #                          )
 stdp = sim.STDPMechanism(timing_dependence=timing_rule,
                          weight_dependence=weight_rule,
-                         weight=RandomDistribution(distribution='normal_clipped', low=0, high=0.2, mu=0.5, sigma=0.3),
+                         weight=RandomDistribution(distribution='normal_clipped', low=0.1, high=0.2, mu=0.5, sigma=0.3),
                          delay=1.0
                          )
 connections_XeAe = sim.Projection(presynaptic_population = input_groups_Xe,
