6513c9e2bcd91e392287256ce8ab05d35416eef3
SakuraLXY <79798356+SakuraLXY@users.noreply.github.com>
2021-08-13 14:48:44 +0800

    Update train1.py

--- train1.py
+++ train1.py
@@ -180,7 +180,7 @@ np.random.seed(0)  # 使得后续生产的随机数可预测
 data_path = './'
 
 weight_path = data_path + 'random/'
-num_examples = 300  #  一次使用训练例子的数量。再多就不行了
+num_examples = 200  #  一次使用训练例子的数量。再多就不行了
 turns=0 # 这是第几次训练
 
 ending = ''
@@ -296,7 +296,7 @@ for one_x_data in train_data:
             else:
             # label_spike_array[k * (n_e // 10) + j].append(7 + one_cnt * (single_example_time + resting_time)) # 对于那些不该响应这个数字的，我们让它在接受图片前就激活
                 label_spike_array[k * (n_e // 10) + j].append(
-                    10 + one_cnt * (single_example_time + resting_time))  # 对于那些不该响应这个数字的，我们让它在接受图片前就激活
+                    12 + one_cnt * (single_example_time + resting_time))  # 对于那些不该响应这个数字的，我们让它在接受图片前就激活
     one_cnt += 1
 for one_x_data in test_data: #最后加一百个作为测试的
     label=one_x_data['output']
