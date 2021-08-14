1a6b86c5b2a4625345e47c1e4cb4810d9a812236
SakuraLXY <79798356+SakuraLXY@users.noreply.github.com>
2021-08-14 16:27:59 +0800

    Update train1.py

--- train1.py
+++ train1.py
@@ -297,9 +297,8 @@ for one_x_data in train_data:
                     #     20 + one_cnt * (single_example_time + resting_time))  # 对于那些应该响应这个数字的，我们让它在接受图片输入后激活
             else:
             # label_spike_array[k * (n_e // 10) + j].append(7 + one_cnt * (single_example_time + resting_time)) # 对于那些不该响应这个数字的，我们让它在接受图片前就激活
-                if random.randint(0,10)<10:
-                    label_spike_array[k * (n_e // 10) + j].append(
-                        12 + one_cnt * (single_example_time + resting_time))  # 对于那些不该响应这个数字的，我们让它在接受图片前就激活
+                label_spike_array[k * (n_e // 10) + j].append(
+                    12 + one_cnt * (single_example_time + resting_time))  # 对于那些不该响应这个数字的，我们让它在接受图片前就激活
     one_cnt += 1
 for one_x_data in test_data: #最后加一百个作为测试的
     label=one_x_data['output']
